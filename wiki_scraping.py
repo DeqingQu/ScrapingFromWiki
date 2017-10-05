42200c0f55d03055fff39e74c4cd3764cae68a21
Alex <alex@10-248-21-57.wireless.oregonstate.edu>
2017-10-05 16:46:49 -0700

    first prototype version

---
+++ wiki_scraping.py
@@ -0,0 +1,64 @@
+import requests
+from time import sleep
+from bs4 import BeautifulSoup
+from datetime import datetime
+
+TARGET_URL = "https://en.wikipedia.org/wiki/Python_(programming_language)"
+BASE_URL = "https://en.wikipedia.org"
+SEARCH_URL = "https://en.wikipedia.org/"
+
+
+def retrieve(url: str):
+    """retrieves content at the specified url"""
+    print("*", url)
+    sleep(1)  # *never* web scrape faster than 1 request per second
+    r = requests.get(url, verify=False)  # get the HTML; ignore SSL errors (present on this particular site)
+    soup = BeautifulSoup(r.text, "lxml")  # parse the HTML
+    return soup
+
+
+def filter_links(url: str, base_url: str, search_url: str):
+    soup = retrieve(url)
+    links_after_filter = {}
+    links_before_filter = soup.find_all('a')
+    for link in links_before_filter:
+        href_url = str(link.get('href'))
+        #   is it a relevant URL to the BASE url?
+        if href_url.startswith("/") and not href_url.startswith("//"):
+            href_url = base_url + href_url
+        if href_url.startswith(search_url):
+            if not href_url in links_after_filter.keys():
+                links_after_filter[href_url] = href_url
+    return links_after_filter
+
+
+
+def get_date(date_str: str):
+    start_index = len('This page was last edited on ')
+    date_str = date_str[start_index+1:]
+    com_index = date_str.find(',')
+    date_str = date_str[:com_index]
+    modify_date = datetime.strptime(date_str, '%d %B %Y')
+    return modify_date
+
+
+def get_timestamp(links: dict):
+    href_count_in_a_month = 0
+    for link in links:
+        detail_soup = retrieve(link)
+        # print('href --- ' + href)
+        for node in detail_soup.find_all('li'):
+            if str(node.get('id')) == "footer-info-lastmod":
+                time_str = node.string
+                modified_date = get_date(time_str)
+                now = datetime.now()
+                if (now - modified_date).days < 30:
+                    href_count_in_a_month += 1
+                    print(time_str)
+                    print('final result + %d' % href_count_in_a_month)
+    print("Result " + href_count_in_a_month)
+
+
+links = {}
+links = filter_links(TARGET_URL, BASE_URL, SEARCH_URL)
+get_timestamp(links)
